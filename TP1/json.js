9d6bd4a5a9b6abfa9fb778a41ac20f255b37d92e
VillaYamil <164430693+VillaYamil@users.noreply.github.com>
2024-04-05 21:03:43 -0300

    Add files via upload

---
+++ TP1/json.js
@@ -0,0 +1,37 @@
+    const personaje = {
+    "id": 140,
+    "name": "Genital Washer",
+    "status": "Alive",
+    "species": "Human",
+    "type": "",
+    "gender": "Male",
+    "origin": {
+        "name": "Post-Apocalyptic Earth",
+        "url": "https://rickandmortyapi.com/api/location/8"
+    },
+    "location": {
+        "name": "Post-Apocalyptic Earth",
+        "url": "https://rickandmortyapi.com/api/location/8"
+    },
+    "image": "https://rickandmortyapi.com/api/character/avatar/140.jpeg",
+    "episode": [
+        "https://rickandmortyapi.com/api/episode/23"
+    ],
+    "url": "https://rickandmortyapi.com/api/character/140",
+    "created": "2017-12-27T18:47:44.566Z"
+}
+//console.log(personaje);
+//console.log(personaje.name);
+/* const jugador = {
+    "nombre": "Nahuel",
+    "apellido": "Barrios",
+    "apodo": "Perrito",
+    "edad": 28,
+    "clubes": ["San Lorenzo","U. Catolica"],
+    "activo": true,
+    "sueldo": 1000000,99,
+}
+console.log(jugador.clubes[0]);*/
+//document.write("El personaje "+personaje.name+" esta ",personaje.status);
+document.getElementById("nombre").inverthml=perosnaje.name;
+document.getElementById("vivo").inverthml=perosnaje.status;
