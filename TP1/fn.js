e5bf86a03f224d96f07cf4e8f5338f10872dbede
VillaYamil <164430693+VillaYamil@users.noreply.github.com>
2024-04-18 21:10:26 -0300

    Add files via upload
    Agregue TP1 como debería ser

---
+++ TP1/fn.js
@@ -0,0 +1,12 @@
+/*$array = ['pera','manzana','frutilla','banana','durazno'];
+console.log ($array [1]);
+for (i=0; i<$array.length; i++){
+    console.log($array[i])
+} 
+let frutas = ['pera','manzana','frutilla','banana','durazno'];
+//frutas.forEach(fruta => {console.log(fruta);})
+for (const fruta of frutas){
+    if (fruta == "manzana") {
+        console.log(fruta);
+    } 
+}*/
